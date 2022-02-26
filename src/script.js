a96d4c2c6ad2c7b28ae63dd1f48e6d95486a0125
albatraozh <81822900+albatraozh@users.noreply.github.com>
2022-02-26 17:11:40 +0330

    cleaning my code

--- src/script.js
+++ src/script.js
@@ -20,12 +20,7 @@ let allIcons = [
   "thunderstorm",
   "drizzle",
 ];
-
-// api
-let apiKey = "a94ab690eaf15d9347e2d7ea11287c43";
-let firstApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
-axios.get(firstApiUrl).then(showWeather);
-
+//////////////////////// all functions
 function showWeather(response) {
   document.querySelector("span#temp").innerHTML = `${Math.round(
     response.data.main.temp
@@ -63,6 +58,71 @@ function showWeather(response) {
     }
   }
 }
+function searchCity(event) {
+  event.preventDefault();
+
+  if (input.value) {
+    let city = document.querySelector("div.city-info h2");
+    input.value = input.value.trim().toLowerCase();
+    city.innerHTML = `<em><strong>${
+      input.value.charAt(0).toUpperCase() + input.value.slice(1)
+    }</strong></em>`;
+  } else {
+    alert("please enter a city");
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+
+  input.value = "";
+}
+function findMyLoc(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+  function retrievePosition(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let myUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    axios.get(myUrl).then(showWeather);
+    axios.get(myUrl).then(function myCityName(response) {
+      document.querySelector(
+        "div.city-info h2"
+      ).innerHTML = `<em><strong>${response.data.name}</strong></em>`;
+    });
+  }
+}
+function changeF(event) {
+  event.preventDefault();
+
+  FchangeFormula("span#temp", mainCtemp);
+  FchangeFormula("#max-temp", maxMainTemp);
+  FchangeFormula("#min-temp", minMainTemp);
+
+  fLink.classList.add("isDisabled");
+  cLink.classList.remove("isDisabled");
+}
+function changeC(event) {
+  event.preventDefault();
+
+  CchangeFormula("span#temp", mainCtemp);
+  CchangeFormula("#max-temp", maxMainTemp);
+  CchangeFormula("#min-temp", minMainTemp);
+
+  cLink.classList.add("isDisabled");
+  fLink.classList.remove("isDisabled");
+}
+function FchangeFormula(place, temp) {
+  let Fweather = (temp * 9) / 5 + 32;
+  document.querySelector(place).innerHTML = `${Math.round(Fweather)}°`;
+}
+function CchangeFormula(place, temp) {
+  document.querySelector(place).innerHTML = `${Math.round(temp)}°`;
+}
+/////////////// api
+let apiKey = "a94ab690eaf15d9347e2d7ea11287c43";
+let firstApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+axios.get(firstApiUrl).then(showWeather);
+
 //showing the time
 let now = new Date();
 
@@ -83,43 +143,12 @@ if (now.getMinutes() > 9 && now.getHours() > 9) {
 let input = document.querySelector("input.form-control");
 let submitCity = document.querySelector(".submit-city");
 
-submitCity.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  if (input.value) {
-    let city = document.querySelector("div.city-info h2");
-    input.value = input.value.trim().toLowerCase();
-    city.innerHTML = `<em><strong>${
-      input.value.charAt(0).toUpperCase() + input.value.slice(1)
-    }</strong></em>`;
-  } else {
-    alert("please enter a city");
-  }
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showWeather);
-
-  input.value = "";
-});
+submitCity.addEventListener("click", searchCity);
 //find my city
 
 let findMyCity = document.querySelector(".my-city");
 
-findMyCity.addEventListener("click", function (event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(retrievePosition);
-  function retrievePosition(position) {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let myUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-    axios.get(myUrl).then(showWeather);
-    axios.get(myUrl).then(function myCityName(response) {
-      document.querySelector(
-        "div.city-info h2"
-      ).innerHTML = `<em><strong>${response.data.name}</strong></em>`;
-    });
-  }
-});
+findMyCity.addEventListener("click", findMyLoc);
 
 //changing C and F main temp
 
@@ -127,39 +156,10 @@ let fLink = document.querySelector("a.fLink");
 let cLink = document.querySelector("a.cLink");
 cLink.classList.add("isDisabled");
 
-function changeF(event) {
-  event.preventDefault();
-
-  FchangeFormula("span#temp", mainCtemp);
-  FchangeFormula("#max-temp", maxMainTemp);
-  FchangeFormula("#min-temp", minMainTemp);
-
-  fLink.classList.add("isDisabled");
-  cLink.classList.remove("isDisabled");
-}
-
 fLink.addEventListener("click", changeF);
 
-function changeC(event) {
-  event.preventDefault();
-
-  CchangeFormula("span#temp", mainCtemp);
-  CchangeFormula("#max-temp", maxMainTemp);
-  CchangeFormula("#min-temp", minMainTemp);
-
-  cLink.classList.add("isDisabled");
-  fLink.classList.remove("isDisabled");
-}
 cLink.addEventListener("click", changeC);
 
 let mainCtemp = null;
 let maxMainTemp = null;
 let minMainTemp = null;
-
-function FchangeFormula(place, temp) {
-  let Fweather = (temp * 9) / 5 + 32;
-  document.querySelector(place).innerHTML = `${Math.round(Fweather)}°`;
-}
-function CchangeFormula(place, temp) {
-  document.querySelector(place).innerHTML = `${Math.round(temp)}°`;
-}
